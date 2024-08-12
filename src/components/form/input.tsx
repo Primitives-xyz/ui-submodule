9d54ebe473e4ff4e43b7e07ca7cca8c6963efe9c
Paul Giron <pol.giron@gmail.com>
2024-08-12 11:22:28 +0200

    update ui

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -6,11 +6,17 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    // const { error } = useFormField()
+
     return (
       <input
         type={type}
         className={cn(
           'flex h-9 w-full rounded-input border border-input bg-background px-3 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          {
+            // 'border-destructive': !!error,
+            // 'focus-visible:ring-destructive border-destructive': !!error,
+          },
           className,
         )}
         ref={ref}
