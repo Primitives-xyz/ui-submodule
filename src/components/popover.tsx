cc0db9e5a62dd67e184cd8abf4aa50abebf960a6
Paul Giron <pol.giron@gmail.com>
2024-08-20 18:24:39 +0200

    update

--- src/components/popover.tsx
+++ src/components/popover.tsx
@@ -13,6 +13,7 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
     sameWidthAsTrigger?: boolean
+    preventAutoFocus?: boolean
   }
 >(
   (
@@ -21,6 +22,7 @@ const PopoverContent = React.forwardRef<
       align = 'center',
       sideOffset = 4,
       sameWidthAsTrigger = false,
+      preventAutoFocus = false,
       ...props
     },
     ref,
@@ -30,6 +32,11 @@ const PopoverContent = React.forwardRef<
         ref={ref}
         align={align}
         sideOffset={sideOffset}
+        onOpenAutoFocus={(event) => {
+          if (preventAutoFocus) {
+            event.preventDefault()
+          }
+        }}
         className={cn(
           'z-50 w-52 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           {
