d8430154db227ca7d5a6e87af562710a9e0e80dc
Paul Giron <pol.giron@gmail.com>
2024-08-20 18:56:53 +0200

    fix transaction card

--- src/components/card-transaction-entry.tsx
+++ src/components/card-transaction-entry.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import { Card } from './card'
+import { Card, CardContent } from './card'
 
 interface Props {
   title: ReactNode
@@ -17,22 +17,24 @@ export function CardTransactionEntry({
   subPrice,
 }: Props) {
   return (
-    <Card className="flex gap-3 items-center">
-      {thumb && <div className="flex items-center">{thumb}</div>}
-      <div className="flex flex-col gap-2">
-        <div className="font-bold text-md">{title}</div>
-        {subtitle && (
-          <div className="text-sm text-muted-foreground">{subtitle}</div>
-        )}
-      </div>
-      <div className="text-right ml-auto">
-        <div className="h-full flex justify-between flex-col">
-          <div className="text-base">{price}</div>
-          {subPrice && (
-            <div className="text-muted-foreground text-sm">{subPrice}</div>
+    <Card>
+      <CardContent className="flex gap-3 items-center">
+        {thumb && <div className="flex items-center">{thumb}</div>}
+        <div className="flex flex-col gap-2">
+          <div className="font-bold text-md">{title}</div>
+          {subtitle && (
+            <div className="text-sm text-muted-foreground">{subtitle}</div>
           )}
         </div>
-      </div>
+        <div className="text-right ml-auto">
+          <div className="h-full flex justify-between flex-col">
+            <div className="text-base">{price}</div>
+            {subPrice && (
+              <div className="text-muted-foreground text-sm">{subPrice}</div>
+            )}
+          </div>
+        </div>
+      </CardContent>
     </Card>
   )
 }
