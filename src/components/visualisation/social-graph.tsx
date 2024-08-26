3ebed67c6ad9964c4c4b4f3ce5d63950b76b32d1
Paul Giron <pol.giron@gmail.com>
2024-08-26 15:45:19 +0200

    fix graph

--- src/components/visualisation/social-graph.tsx
+++ src/components/visualisation/social-graph.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { SigmaContainer } from '@react-sigma/core'
+import '@react-sigma/core/lib/react-sigma.min.css'
 import { SocialGraphContent, SocialGraphProps } from './social-graph-content'
 import { SocialGraphControls } from './social-graph-controls'
 import { SocialGraphLayout } from './social-graph-layout'
@@ -12,14 +13,10 @@ export default function SocialGraph({
   return (
     <SigmaContainer
       className="w-full h-full !bg-transparent"
-      // style={{
-      //   width: '100%',
-      //   height: '100%',
+      // settings={{
+      //   allowInvalidContainer: true,
+      //   // renderLabels: false,
       // }}
-      settings={{
-        allowInvalidContainer: true,
-        // renderLabels: false,
-      }}
     >
       <SocialGraphContent nodes={nodes} relationships={relationships} />
       <SocialGraphLayout />
