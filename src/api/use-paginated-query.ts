85ff2f3e614d55bd9e2143ce12c40d70629e6455
Cedrick <monsieur.cedrick@gmail.com>
2024-10-10 09:37:56 +0200

    load more observer

--- src/api/use-paginated-query.ts
+++ src/api/use-paginated-query.ts
@@ -1,100 +1,51 @@
-import useSWRInfinite, { SWRInfiniteConfiguration } from 'swr/infinite'
+import useSWRInfinite, { SWRInfiniteResponse } from 'swr/infinite'
 import { FetchMethod } from './api.models'
-import {
-  fetchWrapper,
-  getUrlWithPathParameters,
-  getUrlWithQueryParameters,
-} from './fetch-wrapper'
+import { fetchWrapper, getUrlWithQueryParameters } from './fetch-wrapper'
 
-interface UsePaginatedQueryProps {
-  endpoint: string
-  pathParams?: Record<string, string>
-  queryParams?: Record<string, string>
-  config?: SWRInfiniteConfiguration
-  toBackend?: boolean
-  getJwt?: () => Promise<string | undefined>
-  pageSize?: number
-  skip?: boolean
+interface Props<T = any> {
+  endpoint: string | null
+  params?: T
 }
 
-export function usePaginatedQuery<
-  ResponseType extends {
-    contents: any[]
-    page: number
-    pageSize: number
-  } = any,
-  Error = unknown,
->({
-  endpoint: _endpoint,
-  queryParams,
-  pathParams,
-  config,
-  toBackend = true,
-  pageSize = 10,
-  skip = false,
-  getJwt,
-}: UsePaginatedQueryProps): ReturnType<typeof useSWRInfinite | any> {
-  const shouldFetch = !!_endpoint && !skip
-
-  const getKey = (pageIndex: number, previousPageData: ResponseType | null) => {
-    if (
-      !shouldFetch ||
-      (previousPageData && previousPageData.contents.length === 0)
-    )
+export function usePaginatedQuery<ResponseType, InputType>({
+  endpoint,
+  params,
+}: Props<InputType>): any | SWRInfiniteResponse<ResponseType, Error> {
+  const getKey = (pageIndex: number) => {
+    if (endpoint) {
+      return getUrlWithQueryParameters(endpoint, {
+        page: pageIndex.toString(),
+        ...params,
+      })
+    } else {
       return null
-
-    let endpoint = _endpoint
-    endpoint = getUrlWithPathParameters({ endpoint, pathParams })
-
-    const paginatedParams = {
-      ...queryParams,
-      page: pageIndex.toString(),
-      pageSize: pageSize.toString(),
     }
-
-    return getUrlWithQueryParameters(endpoint, paginatedParams)
   }
 
-  const { data, error, size, isValidating, setSize, mutate } = useSWRInfinite<
+  const { data, size, setSize, error, isLoading, mutate } = useSWRInfinite<
     ResponseType,
     Error
   >(
     getKey,
-    async (endpoint: string) =>
+    (endpoint: string) =>
       fetchWrapper<ResponseType>({
         method: FetchMethod.GET,
         endpoint,
-        toBackend,
-        jwt: getJwt ? await getJwt() : undefined,
       }),
     {
-      ...config,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-      revalidateFirstPage: true,
+      revalidateFirstPage: false,
     },
   )
 
-  const paginatedData = data
-    ? data
-        .filter((page) => page && page.contents)
-        .flatMap((page) => page.contents)
-    : []
-
-  const isLoadingInitialData = !data && isValidating
   const isLoadingMore =
-    isValidating && size > 0 && typeof data?.[size - 1] === 'undefined'
-
-  const hasMore = data ? size !== pageSize : false
+    isLoading || (size > 0 && data && typeof data[size - 1] === 'undefined')
 
   return {
-    data: paginatedData,
+    data,
+    page: size,
     error,
-    size,
-    isLoading: isLoadingInitialData,
-    isLoadingMore,
-    hasMore,
-    setSize,
+    loading: !!isLoadingMore,
+    setPage: setSize,
     refetch: mutate,
   }
 }
