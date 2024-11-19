5f4bcc068c394d3bea7d762cf580f97dc495f682
Paul Giron <pol.giron@gmail.com>
2024-11-19 16:41:07 +0100

    update

--- src/tapestry/hooks/use-get-profile-info.ts
+++ src/tapestry/hooks/use-get-profile-info.ts
@@ -5,8 +5,7 @@ import { IGetProfileResponse } from '../models'
 
 export const useGetProfileInfo = ({ username }: { username: string }) => {
   const { data, error, loading, refetch } = useQuery<IGetProfileResponse>({
-    endpoint: `/profiles/${username}`,
-    bypassCache: true,
+    endpoint: `profiles/${username}`,
   })
 
   return {
