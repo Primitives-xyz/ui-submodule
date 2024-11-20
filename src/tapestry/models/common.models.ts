775e3b338f4c604df35b8c0fa52f1cede2d198d9
Paul Giron <pol.giron@gmail.com>
2024-11-20 14:45:13 +0100

    update types

--- src/tapestry/models/common.models.ts
+++ src/tapestry/models/common.models.ts
@@ -2,3 +2,8 @@ export interface IPaginatedResponse {
   page: number
   pageSize: number
 }
+
+export interface ISocialCounts {
+  likeCount: number
+  commentCount: number
+}
--- src/tapestry/models/contents.models.ts
+++ src/tapestry/models/contents.models.ts
@@ -1,6 +1,35 @@
+import { IPaginatedResponse, ISocialCounts } from './common.models'
+import { IProfile } from './profiles.models'
+
+export interface IContent {
+  namespace: string
+  created_at: number
+  id: string
+  // appNamespace: string
+}
+
+export interface IRequestingProfileSocialInfo {}
+
 // POST /contents/findOrCreate
 
 export interface IFindOrCreateContentInput {
   id: string
   profileId: string
+  properties: [
+    {
+      key: string
+      value: string
+    },
+  ]
+}
+
+// GET /contents
+
+export interface IGetContentsResponse extends IPaginatedResponse {
+  contents: {
+    authorProfile: IProfile
+    content: IContent
+    socialCounts: ISocialCounts
+    requestingProfileSocialInfo: IRequestingProfileSocialInfo
+  }[]
 }
