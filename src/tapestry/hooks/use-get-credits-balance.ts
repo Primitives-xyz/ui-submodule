a27a69f9f17c182ad6af9b2d64139a75f9b8c1c3
Cedrick <monsieur.cedrick@gmail.com>
2025-02-03 14:43:44 +0100

    useGetCredtisBalance

--- src/tapestry/hooks/use-get-credits-balance.ts
+++ src/tapestry/hooks/use-get-credits-balance.ts
@@ -4,9 +4,10 @@ import { useQuery } from '../../api'
 import { IGetCreditsBalanceResponse } from '../models'
 
 export const useGetCreditsBalance = () => {
-  const { data, error, loading, refetch } = useQuery<IGetCreditsBalanceResponse>({
-    endpoint: `/shared/credits/balance`,
-  })
+  const { data, error, loading, refetch } =
+    useQuery<IGetCreditsBalanceResponse>({
+      endpoint: `/shared/credits/balance`,
+    })
 
   return {
     data,
