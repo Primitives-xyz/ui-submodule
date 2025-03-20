81d68defef81cfbf2f71dd773621277376ceed0a
Paul Giron <pol.giron@gmail.com>
2025-03-20 15:46:34 +0100

    update components

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -30,7 +30,7 @@ const buttonVariants = cva(cn(buttonBase, 'rounded-button font-semibold'), {
     size: {
       default: 'h-10 px-4 py-2',
       sm: 'h-7 px-3.5 text-xs',
-      lg: 'h-16 px-12 text-xl font-semibold uppercase',
+      lg: 'h-15 px-12 text-xl font-semibold uppercase',
       icon: 'h-10 w-10',
       icon_sm: 'h-6 w-6',
       icon_lg: 'h-11 w-11',
